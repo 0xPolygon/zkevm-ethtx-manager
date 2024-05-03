21f47797507970c0be00c7f437f93d724c76c134
Toni Ramírez <toni@polygon.technology>
2024-05-03 09:08:20 +0200

    fix description

--- ethtxmanager/config.go
+++ ethtxmanager/config.go
@@ -61,7 +61,7 @@ type Config struct {
 	// PersistenceFilename is the filename to store the memory storage
 	PersistenceFilename string `mapstructure:"PersistenceFilename"`
 	// ReadPendingL1Txs is a flag to enable the reading of pending L1 txs
-	// It shouldn't be enabled if PersistenceFilename is not empty
+	// It can only be enabled if PersistenceFilename empty
 	ReadPendingL1Txs bool `mapstructure:"ReadPendingL1Txs"`
 	// Etherman configuration
 	Etherman etherman.Config `mapstructure:"Etherman"`
